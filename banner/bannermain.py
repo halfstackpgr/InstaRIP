6f195f4638dbfc6046470a356b13694422eade52
Parth Mishra <halfstackpgr@gmail.com>
2022-12-09 12:32:06 +0530

    Update to Script

---
+++ banner/bannermain.py
@@ -0,0 +1,736 @@
+import colorama
+import os
+import time
+import sys
+from sys import platform
+from colorama import Fore
+
+if platform == "linux" or platform == "linux2":
+    outs="clear"
+    # linux
+elif platform == "win32":
+    outs="cls"
+
+
+#Defining a function to call it later while animating the loop.
+
+
+
+def rotationOne():
+#Rotation One
+    earth1=open('animation_cli\earthro1.txt', 'r')
+    readanimation1 = earth1.read()
+    print(Fore.GREEN+readanimation1)
+
+
+
+#Rotation Two
+def rotationTwo():
+    earth2=open('animation_cli\earthro2.txt', 'r')
+    readanimation2 = earth2.read()
+    print(Fore.GREEN+readanimation2)
+
+
+
+#Rotation Three
+def rotationThree():
+    earth3=open('animation_cli\earthro3.txt', 'r')
+    readanimation3 = earth3.read()
+    print(Fore.GREEN+readanimation3)
+
+
+
+#Rotation Four
+def rotationFour():
+    earth4=open('animation_cli\earthro4.txt', 'r')
+    readanimation4 = earth4.read()
+    print(Fore.GREEN+readanimation4)
+
+
+#Rotation Five
+def rotationFive():
+    earth5=open('animation_cli\earthro5.txt', 'r')
+    readanimation5 = earth5.read()
+    print(Fore.GREEN+readanimation5)
+
+
+
+#Rotation Six
+def rotationSix():
+    earth6=open('animation_cli\earthro6.txt', 'r')
+    readanimation6 = earth6.read()
+    print(Fore.GREEN+readanimation6)
+
+
+
+
+#Rotation Seven
+def rotationSeven():
+    earth7=open('animation_cli\earthro7.txt', 'r')
+    readanimation7 = earth7.read()
+    print(Fore.GREEN+readanimation7)
+
+
+
+
+#Rotation Eight
+def rotationEight():
+    earth8=open('animation_cli\earthro8.txt', 'r')
+    readanimation8 = earth8.read()
+    print(Fore.GREEN+readanimation8)
+
+
+
+
+#Rotation Nine
+def rotationNine():
+    earth9=open('animation_cli\earthro9.txt', 'r')
+    readanimation9 = earth9.read()
+    print(Fore.GREEN+readanimation9)
+
+
+
+
+#Rotation Ten
+def rotationTen():
+    earth10=open('animation_cli\earthro10.txt', 'r')
+    readanimation10 = earth10.read()
+    print(Fore.GREEN+readanimation10)
+
+
+
+#Rotation Eleven
+def rotationEleven():
+    earth11=open('animation_cli\earthro11.txt', 'r')
+    readanimation11 = earth11.read()
+    print(Fore.GREEN+readanimation11)
+
+
+
+
+#Rotation Twelve
+def rotationTwelve():
+    earth12=open('animation_cli\earthro12.txt', 'r')
+    readanimation12 = earth12.read()
+    print(Fore.GREEN+readanimation12)
+
+
+
+#Rotation Thirteen
+def rotationThirteen():
+    earth13=open('animation_cli\earthro13.txt', 'r')
+    readanimation13 = earth13.read()
+    print(Fore.GREEN+readanimation13)
+
+
+
+
+#Rotation Fourteen
+def rotationFourteen():
+    earth14=open('animation_cli\earthro14.txt', 'r')
+    readanimation14 = earth14.read()
+    print(Fore.GREEN+readanimation14)
+
+
+
+
+
+#Rotation Fifteen
+def rotationFifteen():
+    earth15=open('animation_cli\earthro15.txt', 'r')
+    readanimation15 = earth15.read()
+    print(Fore.GREEN+readanimation15)
+
+
+
+
+
+#Rotation Sixteen
+def rotationSixteen():
+    earth16=open('animation_cli\earthro16.txt', 'r')
+    readanimation16 = earth16.read()
+    print(Fore.GREEN+readanimation16)
+
+
+
+
+
+#Rotation Seventeen
+def rotationSeventeen():
+    earth17=open('animation_cli\earthro17.txt', 'r')
+    readanimation17 = earth17.read()
+    print(Fore.GREEN+readanimation17)
+
+
+
+
+#Rotation Eighteen
+def rotationEighteen():
+    earth18=open('animation_cli\earthro18.txt', 'r')
+    readanimation18 = earth18.read()
+    print(Fore.GREEN+readanimation18)
+
+
+
+
+
+#Rotation Nineteen
+def rotationNineteen():
+    earth19=open('animation_cli\earthro19.txt', 'r')
+    readanimation19 = earth19.read()
+    print(Fore.GREEN+readanimation19)
+
+
+
+
+
+#Rotation Twenty
+def rotationTwenty():
+    earth20=open('animation_cli\earthro20.txt', 'r')
+    readanimation20 = earth20.read()
+    print(Fore.GREEN+readanimation20)
+
+
+
+
+
+#Rotation Twentyone
+def rotationTenwtyone():
+    earth21=open('animation_cli\earthro21.txt', 'r')
+    readanimation21 = earth21.read()
+    print(Fore.GREEN+readanimation21)
+
+
+
+
+
+#Rotation Twentytwo
+def rotationTwentytwo():
+    earth22=open('animation_cli\earthro22.txt', 'r')
+    readanimation22 = earth22.read()
+    print(Fore.GREEN+readanimation22)
+
+
+
+
+
+#Rotation Twentythree
+def rotationTwentythree():
+    earth23=open('animation_cli\earthro23.txt', 'r')
+    readanimation23 = earth23.read()
+    print(Fore.GREEN+readanimation23)
+
+
+
+
+
+
+#Rotation Twentyfour
+def rotationTwentyfour():
+    earth24=open('animation_cli\earthro24.txt', 'r')
+    readanimation24 = earth24.read()
+    print(Fore.GREEN+readanimation24)
+
+
+
+
+
+#Rotation TwentyFive
+def rotationTwentyfive():
+    earth25=open('animation_cli\earthro25.txt', 'r')
+    readanimation25 = earth25.read()
+    print(Fore.GREEN+readanimation25)
+
+
+
+def earthrotationsystem():
+    rotationOne()
+    os.system(outs)
+    rotationTwo()
+    os.system(outs)
+    rotationThree()
+    os.system(outs)
+    rotationFour()
+    os.system(outs)
+    rotationFive()
+    os.system(outs)
+    rotationFive()
+    os.system(outs)
+    rotationSix()
+    os.system(outs)
+    rotationSeven()
+    os.system(outs)
+    rotationEight()
+    os.system(outs)
+    rotationNine()
+    os.system(outs)
+    rotationTen()
+    os.system(outs)
+    rotationEleven()
+    os.system(outs)
+    rotationTwelve()
+    os.system(outs)
+    rotationThirteen()
+    os.system(outs)
+    rotationThirteen()
+    os.system(outs)
+    rotationFourteen()
+    os.system(outs)
+    rotationFifteen()
+    os.system(outs)
+    rotationSixteen()
+    os.system(outs)
+    rotationSeventeen()
+    os.system(outs)
+    rotationEighteen()
+    os.system(outs)
+    rotationNineteen()
+    os.system(outs)
+    rotationTwenty()
+    os.system(outs)
+    rotationTenwtyone()
+    os.system(outs)
+    rotationTwentytwo()
+    os.system(outs)
+    rotationTwentythree()
+    os.system(outs)
+    rotationTwentyfour()
+    os.system(outs)
+    rotationTwentyfive()
+    os.system(outs)
+    rotationOne()
+    os.system(outs)
+    rotationTwo()
+    os.system(outs)
+    rotationThree()
+    os.system(outs)
+    rotationFour()
+    os.system(outs)
+    rotationFive()
+    os.system(outs)
+    rotationFive()
+    os.system(outs)
+    rotationSix()
+    os.system(outs)
+    rotationSeven()
+    os.system(outs)
+    rotationEight()
+    os.system(outs)
+    rotationNine()
+    os.system(outs)
+    rotationTen()
+    os.system(outs)
+    rotationEleven()
+    os.system(outs)
+    rotationTwelve()
+    os.system(outs)
+    rotationThirteen()
+    os.system(outs)
+    rotationThirteen()
+    os.system(outs)
+    rotationFourteen()
+    os.system(outs)
+    rotationFifteen()
+    os.system(outs)
+    rotationSixteen()
+    os.system(outs)
+    rotationSeventeen()
+    os.system(outs)
+    rotationEighteen()
+    os.system(outs)
+    rotationNineteen()
+    os.system(outs)
+    rotationTwenty()
+    os.system(outs)
+    rotationTenwtyone()
+    os.system(outs)
+    rotationTwentytwo()
+    os.system(outs)
+    rotationTwentythree()
+    os.system(outs)
+    rotationTwentyfour()
+    os.system(outs)
+    rotationTwentyfive()
+    os.system(outs)
+    rotationOne()
+    os.system(outs)
+    rotationTwo()
+    os.system(outs)
+    rotationThree()
+    os.system(outs)
+    rotationFour()
+    os.system(outs)
+    rotationFive()
+    os.system(outs)
+    rotationFive()
+    os.system(outs)
+    rotationSix()
+    os.system(outs)
+    rotationSeven()
+    os.system(outs)
+    rotationEight()
+    os.system(outs)
+    rotationNine()
+    os.system(outs)
+    rotationTen()
+    os.system(outs)
+    rotationEleven()
+    os.system(outs)
+    rotationTwelve()
+    os.system(outs)
+    rotationThirteen()
+    os.system(outs)
+    rotationThirteen()
+    os.system(outs)
+    rotationFourteen()
+    os.system(outs)
+    rotationFifteen()
+    os.system(outs)
+    rotationSixteen()
+    os.system(outs)
+    rotationSeventeen()
+    os.system(outs)
+    rotationEighteen()
+    os.system(outs)
+    rotationNineteen()
+    os.system(outs)
+    rotationTwenty()
+    os.system(outs)
+    rotationTenwtyone()
+    os.system(outs)
+    rotationTwentytwo()
+    os.system(outs)
+    rotationTwentythree()
+    os.system(outs)
+    rotationTwentyfour()
+    os.system(outs)
+    rotationTwentyfive()
+    os.system(outs)
+    rotationOne()
+    os.system(outs)
+    rotationTwo()
+    os.system(outs)
+    rotationThree()
+    os.system(outs)
+    rotationFour()
+    os.system(outs)
+    rotationFive()
+    os.system(outs)
+    rotationFive()
+    os.system(outs)
+    rotationSix()
+    os.system(outs)
+    rotationSeven()
+    os.system(outs)
+    rotationEight()
+    os.system(outs)
+    rotationNine()
+    os.system(outs)
+    rotationTen()
+    os.system(outs)
+    rotationEleven()
+    os.system(outs)
+    rotationTwelve()
+    os.system(outs)
+    rotationThirteen()
+    os.system(outs)
+    rotationThirteen()
+    os.system(outs)
+    rotationFourteen()
+    os.system(outs)
+    rotationFifteen()
+    os.system(outs)
+    rotationSixteen()
+    os.system(outs)
+    rotationSeventeen()
+    os.system(outs)
+    rotationEighteen()
+    os.system(outs)
+    rotationNineteen()
+    os.system(outs)
+    rotationTwenty()
+    os.system(outs)
+    rotationTenwtyone()
+    os.system(outs)
+    rotationTwentytwo()
+    os.system(outs)
+    rotationTwentythree()
+    os.system(outs)
+    rotationTwentyfour()
+    os.system(outs)
+    rotationTwentyfive()
+    os.system(outs)
+    rotationOne()
+    os.system(outs)
+    rotationTwo()
+    os.system(outs)
+    rotationThree()
+    os.system(outs)
+    rotationFour()
+    os.system(outs)
+    rotationFive()
+    os.system(outs)
+    rotationFive()
+    os.system(outs)
+    rotationSix()
+    os.system(outs)
+    rotationSeven()
+    os.system(outs)
+    rotationEight()
+    os.system(outs)
+    rotationNine()
+    os.system(outs)
+    rotationTen()
+    os.system(outs)
+    rotationEleven()
+    os.system(outs)
+    rotationTwelve()
+    os.system(outs)
+    rotationThirteen()
+    os.system(outs)
+    rotationThirteen()
+    os.system(outs)
+    rotationFourteen()
+    os.system(outs)
+    rotationFifteen()
+    os.system(outs)
+    rotationSixteen()
+    os.system(outs)
+    rotationSeventeen()
+    os.system(outs)
+    rotationEighteen()
+    os.system(outs)
+    rotationNineteen()
+    os.system(outs)
+    rotationTwenty()
+    os.system(outs)
+    rotationTenwtyone()
+    os.system(outs)
+    rotationTwentytwo()
+    os.system(outs)
+    rotationTwentythree()
+    os.system(outs)
+    rotationTwentyfour()
+    os.system(outs)
+    rotationTwentyfive()
+    os.system(outs)
+    rotationOne()
+    os.system(outs)
+    rotationTwo()
+    os.system(outs)
+    rotationThree()
+    os.system(outs)
+    rotationFour()
+    os.system(outs)
+    rotationFive()
+    os.system(outs)
+    rotationFive()
+    os.system(outs)
+    rotationSix()
+    os.system(outs)
+    rotationSeven()
+    os.system(outs)
+    rotationEight()
+    os.system(outs)
+    rotationNine()
+    os.system(outs)
+    rotationTen()
+    os.system(outs)
+    rotationEleven()
+    os.system(outs)
+    rotationTwelve()
+    os.system(outs)
+    rotationThirteen()
+    os.system(outs)
+    rotationThirteen()
+    os.system(outs)
+    rotationFourteen()
+    os.system(outs)
+    rotationFifteen()
+    os.system(outs)
+    rotationSixteen()
+    os.system(outs)
+    rotationSeventeen()
+    os.system(outs)
+    rotationEighteen()
+    os.system(outs)
+    rotationNineteen()
+    os.system(outs)
+    rotationTwenty()
+    os.system(outs)
+    rotationTenwtyone()
+    os.system(outs)
+    rotationTwentytwo()
+    os.system(outs)
+    rotationTwentythree()
+    os.system(outs)
+    rotationTwentyfour()
+    os.system(outs)
+    rotationTwentyfive()
+    os.system(outs)
+    rotationOne()
+    os.system(outs)
+    rotationTwo()
+    os.system(outs)
+    rotationThree()
+    os.system(outs)
+    rotationFour()
+    os.system(outs)
+    rotationFive()
+    os.system(outs)
+    rotationFive()
+    os.system(outs)
+    rotationSix()
+    os.system(outs)
+    rotationSeven()
+    os.system(outs)
+    rotationEight()
+    os.system(outs)
+    rotationNine()
+    os.system(outs)
+    rotationTen()
+    os.system(outs)
+    rotationEleven()
+    os.system(outs)
+    rotationTwelve()
+    os.system(outs)
+    rotationThirteen()
+    os.system(outs)
+    rotationThirteen()
+    os.system(outs)
+    rotationFourteen()
+    os.system(outs)
+    rotationFifteen()
+    os.system(outs)
+    rotationSixteen()
+    os.system(outs)
+    rotationSeventeen()
+    os.system(outs)
+    rotationEighteen()
+    os.system(outs)
+    rotationNineteen()
+    os.system(outs)
+    rotationTwenty()
+    os.system(outs)
+    rotationTenwtyone()
+    os.system(outs)
+    rotationTwentytwo()
+    os.system(outs)
+    rotationTwentythree()
+    os.system(outs)
+    rotationTwentyfour()
+    os.system(outs)
+    rotationTwentyfive()
+    os.system(outs)
+    rotationOne()
+    os.system(outs)
+    rotationTwo()
+    os.system(outs)
+    rotationThree()
+    os.system(outs)
+    rotationFour()
+    os.system(outs)
+    rotationFive()
+    os.system(outs)
+    rotationFive()
+    os.system(outs)
+    rotationSix()
+    os.system(outs)
+    rotationSeven()
+    os.system(outs)
+    rotationEight()
+    os.system(outs)
+    rotationNine()
+    os.system(outs)
+    rotationTen()
+    os.system(outs)
+    rotationEleven()
+    os.system(outs)
+    rotationTwelve()
+    os.system(outs)
+    rotationThirteen()
+    os.system(outs)
+    rotationThirteen()
+    os.system(outs)
+    rotationFourteen()
+    os.system(outs)
+    rotationFifteen()
+    os.system(outs)
+    rotationSixteen()
+    os.system(outs)
+    rotationSeventeen()
+    os.system(outs)
+    rotationEighteen()
+    os.system(outs)
+    rotationNineteen()
+    os.system(outs)
+    rotationTwenty()
+    os.system(outs)
+    rotationTenwtyone()
+    os.system(outs)
+    rotationTwentytwo()
+    os.system(outs)
+    rotationTwentythree()
+    os.system(outs)
+    rotationTwentyfour()
+    os.system(outs)
+    rotationTwentyfive()
+    os.system(outs)
+    rotationOne()
+    os.system(outs)
+    rotationTwo()
+    os.system(outs)
+    rotationThree()
+    os.system(outs)
+    rotationFour()
+    os.system(outs)
+    rotationFive()
+    os.system(outs)
+    rotationFive()
+    os.system(outs)
+    rotationSix()
+    os.system(outs)
+    rotationSeven()
+    os.system(outs)
+    rotationEight()
+    os.system(outs)
+    rotationNine()
+    os.system(outs)
+    rotationTen()
+    os.system(outs)
+    rotationEleven()
+    os.system(outs)
+    rotationTwelve()
+    os.system(outs)
+    rotationThirteen()
+    os.system(outs)
+    rotationThirteen()
+    os.system(outs)
+    rotationFourteen()
+    os.system(outs)
+    rotationFifteen()
+    os.system(outs)
+    rotationSixteen()
+    os.system(outs)
+    rotationSeventeen()
+    os.system(outs)
+    rotationEighteen()
+    os.system(outs)
+    rotationNineteen()
+    os.system(outs)
+    rotationTwenty()
+    os.system(outs)
+    rotationTenwtyone()
+    os.system(outs)
+    rotationTwentytwo()
+    os.system(outs)
+    rotationTwentythree()
+    os.system(outs)
+    rotationTwentyfour()
+    os.system(outs)
+    rotationTwentyfive()
+    os.system(outs)
+def mainBanner():
+    ServerSystemStart='''
+    '''
+earthrotationsystem()
